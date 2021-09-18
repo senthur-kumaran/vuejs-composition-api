a3903efbe70c53e9d138842eff0228ff504204dd
Senthur Kumaran <senthurkumaran@adventus.io>
2021-09-18 10:01:26 +0530

    Initial commit + compostion API and testing

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<section class="section">
+  <div class="container">
+    <Timeline/>
+  </div>
+</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Timeline from './components/Timeline.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Timeline
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import Timeline from '../../src/components/Timeline.vue'
+import { today, thisWeek, thisMonth } from '../../src/mocks'
+
+describe('Timeline', () => {
+  it('renders today post default', () => {
+    const wrapper = mount(Timeline)
+
+    expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+  })
+
+  it('update when the period is click', async () => {
+    const wrapper = mount(Timeline)
+
+    await wrapper.get('[data-test="This Week"]').trigger('click')
+
+    expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+    expect(wrapper.html()).toContain(thisWeek.created.format('Do MMM'))
+  })
+
+  it('update when the period is click', async () => {
+    const wrapper = mount(Timeline)
+
+    await wrapper.get('[data-test="This Month"]').trigger('click')
+
+    expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+    expect(wrapper.html()).toContain(thisWeek.created.format('Do MMM'))
+    expect(wrapper.html()).toContain(thisMonth.created.format('Do MMM'))
+  })
+})
